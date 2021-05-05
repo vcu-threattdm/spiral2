fb6a5b37a743860e606214db50c85a9fc4755ef8
vcu-threattdm <78376316+vcu-threattdm@users.noreply.github.com>
2021-05-04 20:03:35 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,11 @@
-def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
+def spiralize(number,start=7):
+    number*=number
+    inc=2
+    return_value=7
+    while start<number:
+        for i in range(4):
+            start+=inc
+            return_value+=start
+        inc+=2
     return return_value
+print ( spiralize(804))
